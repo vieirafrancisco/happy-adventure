33f411bd2ec78525a813bafe5c03369a1335e169
jadson <jadsonaluno@hotmail.com>
2018-10-18 00:13:39 -0300

    remove unused files

--- player.py
+++
@@ -1,6 +0,0 @@
-from obj import Object
-
-class Player(Object):
-    
-    def __init__(self, master, pos_x, pos_y, width, height, image_root=None):
-        super().__init__(master, pos_x, pos_y, width, height, image_root)
